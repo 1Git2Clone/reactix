cfec136e742f7e3a03f9449a76daa02cbb3365c6
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-06-07 02:49:13 +0300

    Replaced unwraps with ? and refactored a bit

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,19 +6,17 @@ use api::api_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
 
     // NOTE: This is the reccomended way to implement this, however it requires you entering that
     // SSL certificate password every time.
     //
-    //builder
-    //    .set_private_key_file("cert/key.pem", SslFiletype::PEM)
-    //    .unwrap();
-    //builder.set_certificate_chain_file("cert/cert.pem").unwrap();
+    //builder.set_private_key_file("cert/key.pem", openssl::ssl::SslFiletype::PEM)?;
+    //builder.set_certificate_chain_file("cert/cert.pem")?;
 
     // WARNING: You should use a password (`builder.set_private_key_file()` - refer to the note
     // above) for production deploys.
-    builder.set_certificate_chain_file("cert/cert.pem").unwrap();
+    builder.set_certificate_chain_file("cert/cert.pem")?;
 
     HttpServer::new(|| {
         App::new()
