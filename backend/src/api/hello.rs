30fd843d9756c9f0958592183d542495c23781fa
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-06-07 23:57:04 +0300

    Added some more api stuff

--- backend/src/api/hello.rs
+++ backend/src/api/hello.rs
@@ -1,5 +1,23 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{get, http::header::ContentType, HttpRequest, HttpResponse, Responder, Result};
 
-pub async fn hello<'a>(user: Option<&'a str>) -> impl Responder {
-    HttpResponse::Ok().body(&format!("Hello {}!", user.unwrap_or("User")))
+#[get("/api/hello")]
+pub async fn hello(_req: HttpRequest) -> Result<impl Responder> {
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body("Hello!"))
+}
+
+#[derive(serde::Serialize)]
+struct User {
+    name: String,
+}
+
+#[get("/api/hello/{username}")]
+pub async fn hello_user(username: actix_web::web::Path<String>) -> Result<impl Responder> {
+    let user = User {
+        name: username.to_string(),
+    };
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body(format!("Hello {}!", user.name)))
 }
